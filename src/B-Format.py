b7e7ee6457dc15e2ad6fdb40a44fc75b8d1ae608
Fèlix Ros Atmetlla <felixrosat@gmail.com>
2018-11-17 22:05:01 +0100

    update functions_1
    Update the script using functions and use them

--- src/B-Format.py
+++ src/B-Format.py
@@ -28,7 +28,7 @@ data, samplerate = sf.read(path)
 azimuth = math.pi*3/4
 elevation = 0
 
-amb_order = 1
+amb_ord = 1
 
 norm = 'N3D'
 ch_order = 'ACN'
@@ -40,55 +40,74 @@ Y = data*math.sin(azimuth)*math.cos(elevation)
 Z = data*math.sin(elevation)
 
 #%% Normalization
-
-#Get the number of channels the audio will have
-n_channels = 0
-for x in range(0,amb_order+1):
+def num_channels(amb_order):
+    n_channels = 0
+    for x in range(0,amb_order+1):
         n_channels = n_channels + (2*x + 1)
         
-norm_fact = np.zeros(n_channels)
+    return n_channels
+
+
+#Get the number of channels the audio will have
+n_ch = num_channels(amb_ord)
+
+def norm_factors(n_channels,amb_order,norm):
+    norm_fact = np.zeros(n_channels)
+    norm_id = 0
+    
+    for x in range (0,amb_order+1):
+        for i in range (-x,x+1):
+            if norm == 'N3D':
+                if i == 0:
+                    norm_fact[norm_id] = math.sqrt(2*x + 1)
+                else:
+                    norm_fact[norm_id] = math.sqrt(2*(2*x+1)*(math.factorial(x-i)/math.factorial(x+i)))
+            
+            elif norm == 'FUMA':
+                if x == 0:
+                    norm_fact[norm_id] = 1/math.sqrt(2)
+                else:
+                    norm_fact[norm_id] = 1
+           
+            elif norm == 'SN3D':
+                if i == 0:
+                    norm_fact[norm_id] = 1
+                else:
+                    norm_fact[norm_id] = math.sqrt(2*(math.factorial(x-i)/math.factorial(x+i)))
+            
+            norm_id += 1
+            
+    return norm_fact
+      
+
 
 #Get the normalization factors based on the order, and the normalization desired by user
-norm_id = 0  
-for x in range (0,amb_order+1):
-    for i in range (-x,x+1):
-        if norm == 'N3D':
-            if i == 0:
-                norm_fact[norm_id] = math.sqrt(2*x + 1)
-            else:
-                norm_fact[norm_id] = math.sqrt(2*(2*x+1)*(math.factorial(x-i)/math.factorial(x+i)))
-        
-        elif norm == 'FUMA':
-            if x == 0:
-                norm_fact[norm_id] = 1/math.sqrt(2)
-            else:
-                norm_fact[norm_id] = 1
-       
-        elif norm == 'SN3D':
-            if i == 0:
-                norm_fact[norm_id] = 1
-            else:
-                norm_fact[norm_id] = math.sqrt(2*(math.factorial(x-i)/math.factorial(x+i)))
-        
-        norm_id += 1
+normalization_fact = norm_factors(n_ch,amb_ord,norm)
       
 #Apply the normalization to the audio channels
-W = W*norm_fact[0]   
-Y = Y*norm_fact[1]
-Z = Z*norm_fact[2]
-X = X*norm_fact[3]
+W = W*normalization_fact[0]   
+Y = Y*normalization_fact[1]
+Z = Z*normalization_fact[2]
+X = X*normalization_fact[3]
     
 #Order the channels in the desired format
-if ch_order=='FUMA':
-    audio = np.vstack((W,X,Y,Z))
 
-elif ch_order=='ACN':
-    audio = np.vstack((W,Y,Z,X))
+def order_channels(ch_order,W,X,Y,Z):
+    if ch_order=='FUMA':
+        audio = np.vstack((W,X,Y,Z))
+    
+    elif ch_order=='ACN':
+        audio = np.vstack((W,Y,Z,X))
+    
+    audio = np.transpose(audio)
+    return audio
 
-final_audio = np.transpose(audio)
+        
+audio = order_channels(ch_order,W,X,Y,Z)
+
 
 #Write the audio file
-sf.write('violinsingle_%s_%s(%d, %d).wav'%(norm,ch_order,azimuth*180/math.pi, elevation*180/math.pi),final_audio,samplerate)
+sf.write('violinsingle_%s_%s(%d, %d).wav'%(norm,ch_order,azimuth*180/math.pi, elevation*180/math.pi),audio,samplerate)
 
 #%% Plots
 
@@ -101,9 +120,9 @@ r_y = np.sin(theta)
 r_w = np.ones(theta.size)
 
 #Get the normalized values
-nr_w = r_w*norm_fact[0] 
-nr_x = r_x*norm_fact[3]
-nr_y = r_y*norm_fact[1]
+nr_w = r_w*normalization_fact[0] 
+nr_x = r_x*normalization_fact[3]
+nr_y = r_y*normalization_fact[1]
 
 #Create the subplots and plot
 f, axarr = plt.subplots(2, 2, subplot_kw=dict(projection='polar'))
@@ -131,49 +150,47 @@ axarr[1, 1].set_theta_offset(math.pi*0.5)
 plt.show()
 
 #%%    
-azi = np.arange(0,2*np.pi,0.01)
-#plt.polar(azi,np.cos(azi))
 
-k = np.sin(azi)
-t = 0
-r = 0
-k_pos = np.empty(0)
-k_neg = np.empty(0)
-for x in range(0, k.size):
-    if k[x] >= 0:
-        k_pos = np.append(k_pos,k[x])
-        k_neg = np.append(k_neg,0)
-    else:
-        k_neg = np.append(k_neg,k[x])
-        k_pos = np.append(k_pos,0)
-        
-        
+def signal_gain(gain):
+    gain_pos = np.empty(0)
+    gain_neg = np.empty(0)
+    
+    for x in gain:
+        if x >= 0:
+            gain_pos = np.append(gain_pos,x)
+            gain_neg = np.append(gain_neg,0)
+        else:
+            gain_neg = np.append(gain_neg,x)
+            gain_pos = np.append(gain_pos,0)
+    
+    gain_neg = np.abs(gain_neg)
+    return gain_pos, gain_neg
+
+
+azi = np.arange(0,2*np.pi,0.01)
+
 s = np.cos(azi)
-t = 0
-r = 0
-s_pos = np.empty(0)
-s_neg = np.empty(0)
-for x in range(0, s.size):
-    if s[x] >= 0:
-        s_pos = np.append(s_pos,s[x])
-        s_neg = np.append(s_neg,0)
-    else:
-        s_neg = np.append(s_neg,s[x])
-        s_pos = np.append(s_pos,0)
+k = np.sin(azi)
+
+s_pos, s_neg = signal_gain(s)
+
+k_pos, k_neg = signal_gain(k)
+
+#plt.polar(azi,np.cos(azi))
 
 plt.figure(0)
 plt.plot(azi,s)
 
-plt.figure(1)
-plt.plot(azi,k)
+#plt.figure(1)
+#plt.plot(azi,k)
 
 plt.figure(2)
 plt.polar(azi,s_pos)
-plt.polar(azi,np.abs(s_neg))
+plt.polar(azi,s_neg)
 
-plt.figure(3)
+#plt.figure(3)
 
-plt.polar(azi,k_pos)
-plt.polar(azi,np.abs(k_neg))
+#plt.polar(azi,k_pos)
+#plt.polar(azi,np.abs(k_neg))
 
     
